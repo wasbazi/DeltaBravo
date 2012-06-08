7d9a35cd99353cff47c91e387bb7e2d62f6c37aa
Dan Ambrisco <dambrisco@gmail.com>
2012-06-08 17:15:07 -0500

    Fixes last_insert_id

--- example.php
+++ example.php
@@ -1,33 +1,35 @@
 <?php
 
-include('../db.inc');
+header('Content-type: text/plain');
+
+include('db.inc');
 
 $db = new DatabaseConnection('localhost', 'dbtest', 'dbtest', 'dbtest', 'mysql', 'utf8');
 
 $where = array(
-	array( 'id', 1, '>', 'AND' ),
-	array( 'content', 'zebra', '<' ),
+	array( 'fid', 1, '>', 'AND' ),
+	array( 'fruit', 'mango', '<' ),
 	);
 $order = array(
-	array('content', 'DESC'),
+	array('fruit', 'DESC'),
 	array('id', 'DESC'),
 	);
 
-$results['select'] = $db->select('test1')->execute();
-$results['select/where'] = $db->select('test1')->where('id', 1)->execute();
-$results['select/where/op'] = $db->select('test1')->where('content', 'a%', 'LIKE')->execute();
-$results['select/where/multi'] = $db->select('test1')->where($where)->execute();
-$results['select/join/using'] = $db->select('test1')->join('test2', 'id')->execute();
-$results['select/join/on'] = $db->select('test1')->join('test2', array('test1.id', 'test2.id', '='))->execute();
-$results['select/join/on/multi'] = $db->select('test1')->join('test2', array(array('test1.id', 'test2.id', '='),array('test1.content', 'test2.animal', '>')))->execute();
-$results['select/join/using/where'] = $db->select('test1')->join('test2', 'id')->where('id', 1)->execute();
-$results['select/where/join/using'] = $db->select('test1')->where('id', 2)->join('test2', 'id')->execute();
-$results['select/order'] = $db->select('test1')->order('id', 'DESC')->execute();
-$results['select/order/multi'] = $db->select('test1')->order($order)->execute();
-$results['select/limit'] = $db->select('test1')->limit(2)->execute();
-$results['select/limit/offset'] = $db->select('test1')->limit(2, 2)->execute();
+$results['select'] = $db->select('fruits')->execute();
+$results['select/where'] = $db->select('fruits')->where('fid', 1)->execute();
+$results['select/where/op'] = $db->select('fruits')->where('fruit', 'a%', 'LIKE')->execute();
+$results['select/where/multi'] = $db->select('fruits')->where($where)->execute();
+$results['select/join/using'] = $db->select('fruits')->join('test', 'fid')->execute();
+$results['select/join/on'] = $db->select('fruits')->join('test', array('fruits.fid', 'test.fid', '='))->execute();
+$results['select/join/on/multi'] = $db->select('fruits')->join('animal', array(array('animals.id', 'test.id', '='),array('animals.animals', 'test.fid', '<')))->execute();
+$results['select/join/using/where'] = $db->select('fruits')->join('test', 'id')->where('id', 1)->execute();
+$results['select/where/join/using'] = $db->select('fruits')->where('id', 2)->join('test', 'id')->execute();
+$results['select/order'] = $db->select('fruits')->order('id', 'DESC')->execute();
+$results['select/order/multi'] = $db->select('fruits')->order($order)->execute();
+$results['select/limit'] = $db->select('fruits')->limit(2)->execute();
+$results['select/limit/offset'] = $db->select('fruits')->limit(2, 2)->execute();
 
-$results['desc'] = $db->desc('test1')->execute();
+$results['desc'] = $db->desc('fruits')->execute();
 
 foreach ($results as &$r) {
 	if (!$r) {
